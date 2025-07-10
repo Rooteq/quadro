0408166534a5b1f727a0eedb3ec2c6e6e2d7ce39
Rooteq <szymerut@gmail.com>
2025-07-10 02:06:29 +0200

    basic computed torque controller works for 1 motor xd

--- src/torque_controller.cpp
+++ src/torque_controller.cpp
@@ -1,20 +1,22 @@
+#pragma optimize("", off)
+
 #include <memory>
+#include <cmath>
 #include <filesystem>
 #include <iostream>
 #include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "ament_index_cpp/get_package_share_directory.hpp"
-#include "pinocchio/algorithm/frames.hpp"
+#include "pinocchio/algorithm/rnea.hpp"
 #include "pinocchio/parsers/urdf.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 class ForwardKinematicsNode : public rclcpp::Node
 {
 public:
     ForwardKinematicsNode() : Node("forward_kinematics_node")
     {
-        // Get URDF file path from launch arguments
-        std::string ee_frame_name = "link";  // Default frame name
         
         const auto package_share_path = ament_index_cpp::get_package_share_directory("quadro");
         const auto urdf_path = std::filesystem::path(package_share_path) / "description" / "robot.xacro";
@@ -24,13 +26,36 @@ public:
             
             pinocchio::urdf::buildModel(urdf_path, model_);
             data_ = std::make_unique<pinocchio::Data>(model_);
+            RCLCPP_INFO(this->get_logger(), "joints: %d", model_.njoints);
+
+            RCLCPP_INFO(this->get_logger(), "nq (positions): %d", model_.nq);
+            RCLCPP_INFO(this->get_logger(), "nv (velocities): %d", model_.nv);
             
-            // Get end-effector frame ID
-            ee_frame_id_ = model_.getFrameId(ee_frame_name);
+            q = Eigen::VectorXd::Zero(model_.nq);
+            q_d = Eigen::VectorXd::Zero(model_.nv);
+
+            ref_q = Eigen::VectorXd::Zero(model_.nq);
+            ref_q_d = Eigen::VectorXd::Zero(model_.nv);
+            ref_q_dd = Eigen::VectorXd::Zero(model_.nv);
+
+            v = Eigen::VectorXd::Zero(model_.nv);
+            tau = Eigen::VectorXd::Zero(model_.nv);
+
+            e = Eigen::VectorXd::Zero(model_.nq);
+            e_d = Eigen::VectorXd::Zero(model_.nv);
+
+            Kp = Eigen::MatrixXd::Zero(model_.nq,model_.nq);
+            Kd = Eigen::MatrixXd::Zero(model_.nv,model_.nv);
+
+            Kp.fill(10.0f);
+            Kd.fill(5.0f);
+
+            Kp(0,1) = 0.0f;
+            Kp(1,1) = 0.0f;
+
+            // Kd(0,1) = 0.0f;
             
             RCLCPP_INFO(this->get_logger(), "Robot model loaded from: %s", urdf_path.c_str());
-            RCLCPP_INFO(this->get_logger(), "End-effector frame: %s", ee_frame_name.c_str());
-            RCLCPP_INFO(this->get_logger(), "Number of joints: %d", model_.nq);
         }
         catch (const std::exception& e) {
             RCLCPP_ERROR(this->get_logger(), "Failed to load robot model: %s", e.what());
@@ -42,12 +67,28 @@ public:
             "/joint_states", 10,
             std::bind(&ForwardKinematicsNode::jointStateCallback, this, std::placeholders::_1));
         
-        // No publisher needed - just print to terminal
-        
+        // Create publisher for torque commands
+        torque_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>(
+            "/effort_controller/commands", 10);
         RCLCPP_INFO(this->get_logger(), "Forward kinematics node initialized");
     }
 
 private:
+    void get_trajectory_setpoint() // Replace fill!!!!
+    {
+        double t = this->get_clock()->now().seconds();
+
+        RCLCPP_INFO(this->get_logger(), "time %f", t);
+        double coeff = t*0.20;
+        ref_q[0] = std::sin(coeff);
+        ref_q_d[0] = 0.2*std::cos(coeff);
+        ref_q_dd[0] = -0.04*std::sin(coeff);
+
+        ref_q[1]  = 0.0f;
+        // ref_q_d[1] = 0.0f;
+        // ref_q_dd[1] = 0.0f;
+    }
+
     void jointStateCallback(const sensor_msgs::msg::JointState::SharedPtr msg)
     {
         // Check if we have the right number of joint positions
@@ -59,42 +100,47 @@ private:
         // }
         
         // Convert joint positions to Eigen vector
-        Eigen::VectorXd q(model_.nq);
-        for (size_t i = 0; i < msg->position.size(); ++i) {
+        // Eigen::VectorXd q(model_.nq);
+        // Eigen::VectorXd q_d(model_.nv);
+        // for (size_t i = 0; i < msg->position.size(); ++i) {
+        //     q[i] = msg->position[i];
+        //     q_d[i] = msg->velocity[i];
+        // }
+            // Fill position vector (size 2)
+
+        for (size_t i = 0; i < msg->position.size() && i < static_cast<size_t>(model_.nq); ++i) {
             q[i] = msg->position[i];
         }
         
-        // Compute forward kinematics
-        pinocchio::framesForwardKinematics(model_, *data_, q);
-        
-        // Get end-effector transform
-        const auto& ee_transform = data_->oMf[ee_frame_id_];
-        
-        // Print position and orientation to terminal
-        std::cout << "\n--- End-Effector Pose ---" << std::endl;
-        std::cout << "Position: [" 
-                  << ee_transform.translation().x() << ", "
-                  << ee_transform.translation().y() << ", "
-                  << ee_transform.translation().z() << "]" << std::endl;
-        
-        // Convert rotation matrix to Euler angles for easier reading
-        Eigen::Vector3d euler_angles = ee_transform.rotation().eulerAngles(0, 1, 2); // XYZ order
-        std::cout << "Orientation (Euler XYZ): [" 
-                  << euler_angles.x() << ", "
-                  << euler_angles.y() << ", "
-                  << euler_angles.z() << "]" << std::endl;
-        
-        // Also print rotation matrix
-        std::cout << "Rotation matrix:" << std::endl 
-                  << ee_transform.rotation() << std::endl;
-        
-        std::cout << "Joint positions: [";
-        for (int i = 0; i < q.size(); ++i) {
-            std::cout << q[i];
-            if (i < q.size() - 1) std::cout << ", ";
+        // Fill velocity vector (size 1) 
+        for (size_t i = 0; i < msg->velocity.size() && i < static_cast<size_t>(model_.nv); ++i) {
+            q_d[i] = msg->velocity[i];
         }
-        std::cout << "]" << std::endl;
-        std::cout << "------------------------\n" << std::endl;
+
+        // get_trajectory_setpoint();
+        get_trajectory_setpoint();
+
+        e = ref_q - q;
+        e_d = ref_q_d - q_d;
+
+        Eigen::VectorXd pos_control = Kp * e;  // Size 2
+        
+        // Extract only the velocity-space component (usually the last nv elements)
+        v = ref_q_dd + pos_control.head(model_.nv) + Kd * e_d; 
+
+        // v = ref_q_dd + Kp * e + Kd * e_d; 
+        
+
+        tau = pinocchio::rnea(model_, *data_, q, q_d, v);
+        // Publish torque (size 1)
+        auto torque_msg = std_msgs::msg::Float64MultiArray();
+        torque_msg.data.resize(model_.nv);
+        for (int i = 0; i < model_.nv; ++i) {
+            torque_msg.data[i] = tau[i];
+        }
+        torque_pub_->publish(torque_msg);
+
+
     }
     
     pinocchio::Model model_;
@@ -102,6 +148,23 @@ private:
     pinocchio::FrameIndex ee_frame_id_;
     
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr torque_pub_;
+
+    Eigen::VectorXd q;
+    Eigen::VectorXd q_d;
+
+    Eigen::VectorXd v;
+    Eigen::VectorXd tau;
+
+    Eigen::VectorXd ref_q;
+    Eigen::VectorXd ref_q_d;
+    Eigen::VectorXd ref_q_dd;
+
+    Eigen::MatrixXd Kp;
+    Eigen::MatrixXd Kd;
+
+    Eigen::VectorXd e;
+    Eigen::VectorXd e_d;
 };
 
 int main(int argc, char** argv)
